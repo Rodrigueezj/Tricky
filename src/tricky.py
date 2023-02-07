6050356af23afdf5081ac2fa0e5936d28ea2b1c8
Juan <jdbelloro@unal.edu.co>
2023-02-06 23:33:44 -0500

    board

--- src/tricky.py
+++ src/tricky.py
@@ -1,9 +1,26 @@
-import pygame, sys
+import sys, pygame
+pygame.init()
 
-size = 800, 800
+size = width, height = 900, 900
+speed = [2, 2]
+white = 255, 255, 255
+black = 0,0,0
 
 board = pygame.display.set_mode(size)
+pygame.display.set_caption('Tricky')
+board.fill(white)
 
+def lines():
+    pygame.draw.line(board, black, (0, 300), (900, 300), 15)
+    pygame.draw.line(board, black, (0, 600), (900, 600), 15)
+    pygame.draw.line(board, black, (300, 0), (300, 900), 15)
+    pygame.draw.line(board, black, (600, 0), (600, 900), 15)
+
+lines()
+
+#mainloop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+
+    pygame.display.flip()
