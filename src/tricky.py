93037c48b5ef2401240f9b25d0173807ffb61185
Juan <jdbelloro@unal.edu.co>
2023-02-07 02:39:24 -0500

    draws circles when 1st player plays

--- src/tricky.py
+++ src/tricky.py
@@ -1,27 +1,74 @@
 import sys, pygame
 import numpy as np
+
 pygame.init()
 
-size = width, height = 900, 900
 speed = [2, 2]
 white = 255, 255, 255
 black = 0,0,0
 
-board = pygame.display.set_mode(size)
+surface = pygame.display.set_mode((900,900))
 pygame.display.set_caption('Tricky')
-board.fill(white)
+surface.fill(white)
 
-def lines():
-    pygame.draw.line(board, black, (0, 300), (900, 300), 15)
-    pygame.draw.line(board, black, (0, 600), (900, 600), 15)
-    pygame.draw.line(board, black, (300, 0), (300, 900), 15)
-    pygame.draw.line(board, black, (600, 0), (600, 900), 15)
+class Board:
+    def __init__(self):
+        self.squares = np.zeros((3,3))
+        print(self.squares)
 
-lines()
+    def mark(self, row, col, player):
+        self.squares[row][col] = player
 
-#mainloop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+    def empty(self, row, col):
+        return self.squares[row][col] == 0
 
-    pygame.display.flip()
+
+class Game:
+
+    def __init__(self):
+        self.board = Board()
+        self.player = 1
+        self.lines()
+
+    def lines(self): #board lines
+        pygame.draw.line(surface, black, (0, 300), (900, 300), 15)
+        pygame.draw.line(surface, black, (0, 600), (900, 600), 15)
+        pygame.draw.line(surface, black, (300, 0), (300, 900), 15)
+        pygame.draw.line(surface, black, (600, 0), (600, 900), 15)
+    
+    def next_turn(self):
+        self.player = (self.player % 2) + 1
+
+    def draw(self, row, col):
+        
+        if self.player == 1:
+            start_desc = col * 
+        elif self.player == 2:
+            center  = (col * 300 + 300 // 2 , row * 300 + 300 // 2  )
+            pygame.draw.circle(surface, black, center, 15)
+
+def main():
+    game = Game()
+    board = game.board
+    while True:                                         #   This is always the same on pygame
+        for event in pygame.event.get():                #  
+            if event.type == pygame.QUIT:               #  checks if we have closed the program                                                 
+                pygame.quit()                           #
+                sys.exit()                              #
+                                                        
+                                                        #casting positions into coordinatess
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos                         #position of the click(built in pygame)
+                col = pos[0] // 300
+                row = pos[1] // 300
+
+                if board.empty(row, col):
+                    board.mark(row, col, game.player)
+                    game.draw(row, col)
+                    game.next_turn()
+                    print(board)
+                    
+ 
+        pygame.display.update() #update the display
+
+main()
